bc5f6a31f0c288cacd204910ddfe246d1ccdd740
Massi <m.bouneffa@upfit-services.com>
2017-06-12 17:01:54 +0200

    Fix height of cards

--- src/main/webapp/js/manage_project.js
+++ src/main/webapp/js/manage_project.js
@@ -1,28 +1,28 @@
-var bar = new ProgressBar.Line('#container', {
-  strokeWidth: 2,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#6159a6',
-  trailColor: '#eee',
-  trailWidth: 2,
-  svgStyle: {width: '100%', height: '100%'},
-  text: {
-    style: {
-      // Text color.
-      // Default: same as stroke color (options.color)
-      color: '#999',
-      position: 'absolute',
-      right: '0',
-      top: '20px',
-      padding: 0,
-      margin: 0,
-      transform: null
+var config = {
+    strokeWidth: 2,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#6159a6',
+    trailColor: '#eee',
+    trailWidth: 2,
+    svgStyle: {width: '100%', height: '100%'},
+    text: {
+        style: {
+            color: '#999',
+            position: 'absolute',
+            right: '0',
+            top: '20px',
+            padding: 0,
+            margin: 0,
+            transform: null
+        },
+        autoStyleContainer: false
     },
-    autoStyleContainer: false
-  },
-  step: (state, bar) => {
-    bar.setText(Math.round(bar.value() * 100) + ' %');
-  }
-});
+    step: function (state, bar) {
+        bar.setText(Math.round(bar.value() * 100) + ' %');
+    }
+};
+
+var bar = new ProgressBar.Line('#container', config);
 
 bar.animate(0.5);
